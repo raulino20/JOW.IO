12d8514c9fd216c9d4eac7ef01d8695292ff9e0d
Cauã <92538361+raulino20@users.noreply.github.com>
2023-05-09 14:22:53 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,23 @@
+function clickMenu(){
+    if(navbarNav.style.display == 'block'){
+        navbarNav.style.display = 'none'
+    }else{
+        navbarNav.style.display = 'block'
+    }
+}
+
+function quemSomos(){
+  if(who.style.display == 'block'){
+    who.style.display = 'none'
+  }else{
+    who.style.display ='block'
+  }
+}
+
+function numBer(){
+    if(num.style.display == 'none'){
+        num.style.display ='block'
+    }else{
+        num.style.display ='none'
+    }
+}
